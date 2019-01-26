7e0995c8c6bb8f86da7944f17f4d77f4f1415e40
personalmountains <46615898+personalmountains@users.noreply.github.com>
2019-01-26 13:37:02 -0500

    empty extension

---
+++ CustomTabNames.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using EnvDTE;
+using EnvDTE80;
+using Task = System.Threading.Tasks.Task;
+
+namespace CustomTabNames
+{
+	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+	[ProvideService(typeof(CustomTabNames), IsAsyncQueryable = true)]
+	[ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
+	[ProvideAutoLoad(UIContextGuids80.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)]
+	[Guid(CustomTabNames.Guid)]
+	public sealed class CustomTabNames : AsyncPackage
+	{
+		public const string Guid = "BEE6C21E-FBF8-49B1-A0F8-89D7DFA732EE";
+		private readonly DTE2 dte;
+
+		public CustomTabNames()
+		{
+			this.dte = Package.GetGlobalService(typeof(DTE)) as DTE2;
+		}
+
+		protected override async Task InitializeAsync(
+			CancellationToken ct, IProgress<ServiceProgressData> p)
+		{
+			await this.JoinableTaskFactory.SwitchToMainThreadAsync(ct);
+		}
+	}
+}
