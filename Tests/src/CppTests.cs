e6e141fe7b197eba292f8a60dcc48b45ad18a728
personalmountains <46615898+personalmountains@users.noreply.github.com>
2019-02-02 11:16:37 -0500

    split test contents in preparation for cs tests, should be mostly reusable

--- Tests/src/CppTests.cs
+++ Tests/src/CppTests.cs
@@ -6,187 +6,66 @@ namespace CustomTabNames.Tests
 	[TestClass]
 	public class CppTests
 	{
-		public static VS vs;
-		public static Operations ops;
-		public static Window file;
+		public static Tests tests;
 
 		[ClassInitialize]
 		public static void Init(TestContext _)
 		{
-			vs = Global.VS;
-			ops = Global.Operations;
-			file = ops.OpenFile(Global.CPP, "f.cpp");
+			tests = new Tests("cpp", "f.cpp");
 		}
 
 		[TestMethod]
 		public void AddRemoveProjectsIgnoreSingle()
 		{
-			AssertCaption("cpp::f.cpp");
-
-			using (ops.SetOptionTemp("IgnoreSingleProject", true))
-			{
-				using (ops.RemoveProjectTemp(Global.CS))
-					AssertCaption("::f.cpp");
-
-				AssertCaption("cpp::f.cpp");
-			}
+			tests.AddRemoveProjectsIgnoreSingle(Global.CS);
 		}
 
 		[TestMethod]
 		public void AddRemoveProjectsDontIgnoreSingle()
 		{
-			AssertCaption("cpp::f.cpp");
-
-			using (ops.SetOptionTemp("IgnoreSingleProject", false))
-			{
-				using (ops.RemoveProjectTemp(Global.CS))
-					AssertCaption("cpp::f.cpp");
-
-				AssertCaption("cpp::f.cpp");
-			}
+			tests.AddRemoveProjectsDontIgnoreSingle(Global.CS);
 		}
 
 		[TestMethod]
 		public void RenameProject()
 		{
-			AssertCaption("cpp::f.cpp");
-
-			using (ops.RenameProjectTemp(Global.CPP, "cpp2"))
-				AssertCaption("cpp2::f.cpp");
-
-			AssertCaption("cpp::f.cpp");
+			tests.RenameProject();
 		}
 
 		[TestMethod]
 		public void RenameFolder()
 		{
-			AssertCaption("cpp::f.cpp");
-
-			using (ops.MoveFileTemp(@"test\cpp\f.cpp", @"test\cpp\a\b\c"))
-			{
-				AssertCaption("cpp:a/b/c:f.cpp");
-
-				using (ops.RenameFolderTemp(@"test\cpp\a", "1"))
-				{
-					AssertCaption("cpp:1/b/c:f.cpp");
-
-					using (ops.RenameFolderTemp(@"test\cpp\1\b", "2"))
-					{
-						AssertCaption("cpp:1/2/c:f.cpp");
-
-						using (ops.RenameFolderTemp(@"test\cpp\1\2\c", "3"))
-							AssertCaption("cpp:1/2/3:f.cpp");
-
-						AssertCaption("cpp:1/2/c:f.cpp");
-					}
-
-					AssertCaption("cpp:1/b/c:f.cpp");
-				}
-
-				AssertCaption("cpp:a/b/c:f.cpp");
-			}
+			tests.RenameFolder();
 		}
 
 		[TestMethod]
 		public void MoveFolder()
 		{
-			AssertCaption("cpp::f.cpp");
-
-			using (ops.MoveFileTemp(@"test\cpp\f.cpp", @"test\cpp\a\b\c"))
-			{
-				AssertCaption("cpp:a/b/c:f.cpp");
-
-				using (ops.MoveFolderTemp(@"test\cpp\a\b\c", @"test\cpp"))
-				{
-					AssertCaption("cpp:c:f.cpp");
-
-					using (ops.MoveFolderTemp(@"test\cpp\c", @"test\cpp\a"))
-					{
-						AssertCaption("cpp:a/c:f.cpp");
-
-						using (ops.MoveFolderTemp(@"test\cpp\a\c", @"test\cpp\a\b"))
-							AssertCaption("cpp:a/b/c:f.cpp");
-
-						AssertCaption("cpp:a/c:f.cpp");
-					}
-
-					AssertCaption("cpp:c:f.cpp");
-				}
-
-				AssertCaption("cpp:a/b/c:f.cpp");
-			}
-
-			AssertCaption("cpp::f.cpp");
+			tests.MoveFolder();
 		}
 
 		[TestMethod]
 		public void RenameFile()
 		{
-			AssertCaption("cpp::f.cpp");
-
-			using (ops.RenameFileTemp(@"test\cpp\f.cpp", "f1.cpp"))
-				AssertCaption("cpp::f1.cpp");
-
-			AssertCaption("cpp::f.cpp");
+			tests.RenameFile();
 		}
 
 		[TestMethod]
 		public void MoveFile()
 		{
-			AssertCaption("cpp::f.cpp");
-
-			using (ops.MoveFileTemp(@"test\cpp\f.cpp", @"test\cpp\a"))
-			{
-				AssertCaption("cpp:a:f.cpp");
-
-				using (ops.MoveFileTemp(@"test\cpp\a\f.cpp", @"test\cpp\a\b"))
-				{
-					AssertCaption("cpp:a/b:f.cpp");
-
-					using (ops.MoveFileTemp(@"test\cpp\a\b\f.cpp", @"test\cpp\a\b\c"))
-					{
-						AssertCaption("cpp:a/b/c:f.cpp");
-
-						using (ops.MoveFileTemp(@"test\cpp\a\b\c\f.cpp", @"test\cpp\d\e\f"))
-						{
-							AssertCaption("cpp:d/e/f:f.cpp");
-						}
-
-						AssertCaption("cpp:a/b/c:f.cpp");
-					}
-
-					AssertCaption("cpp:a/b:f.cpp");
-				}
-
-				AssertCaption("cpp:a:f.cpp");
-			}
-
-			AssertCaption("cpp::f.cpp");
+			tests.MoveFile();
 		}
 
 		[TestMethod]
 		public void OpenFile()
 		{
-			file.Close();
-
-			file = ops.OpenFile(Global.CPP, "f.cpp");
-			AssertCaption("cpp::f.cpp");
+			tests.OpenFile(Global.CPP);
 		}
 
 		[TestMethod]
 		public void ToggleExtension()
 		{
-			AssertCaption("cpp::f.cpp");
-
-			using (ops.SetOptionTemp("Enabled", false))
-				AssertCaption("f.cpp");
-
-			AssertCaption("cpp::f.cpp");
-		}
-
-		private void AssertCaption(string s)
-		{
-			Assert.AreEqual(s, file.Caption);
+			tests.ToggleExtension();
 		}
 	}
 }
--- Tests/src/Global.cs
+++ Tests/src/Global.cs
@@ -50,4 +50,198 @@ namespace CustomTabNames.Tests
 	}
 
 
+	public class Tests
+	{
+		private Operations ops;
+		private string project, filename, p;
+		private Window file;
+
+		public Tests(string projectName, string filename)
+		{
+			this.ops = Global.Operations;
+			this.project = projectName;
+			this.filename = filename;
+			this.file = ops.OpenFile(ops.GetProject(projectName), filename);
+
+			// prefix
+			this.p = @"test\" + project;
+		}
+
+		public void AddRemoveProjectsIgnoreSingle(Project otherProject)
+		{
+			AssertCaption("");
+
+			using (ops.SetOptionTemp("IgnoreSingleProject", true))
+			{
+				AssertCaption("");
+
+				using (ops.RemoveProjectTemp(otherProject))
+					AssertCaption("", "", filename);
+
+				AssertCaption("");
+			}
+		}
+
+		public void AddRemoveProjectsDontIgnoreSingle(Project otherProject)
+		{
+			AssertCaption("");
+
+			using (ops.SetOptionTemp("IgnoreSingleProject", false))
+			{
+				AssertCaption("");
+
+				using (ops.RemoveProjectTemp(otherProject))
+					AssertCaption("");
+
+				AssertCaption("");
+			}
+		}
+
+		public void RenameProject()
+		{
+			var to = project + "2";
+
+			AssertCaption("");
+
+			using (ops.RenameProjectTemp(ops.GetProject(project), to))
+				AssertCaption(to, "", filename);
+
+			AssertCaption("");
+		}
+
+		public void RenameFolder()
+		{
+			AssertCaption("");
+
+			using (ops.MoveFileTemp(p + @"\" + filename, p + @"\a\b\c"))
+			{
+				AssertCaption("a/b/c");
+
+				using (ops.RenameFolderTemp(p + "\a", "1"))
+				{
+					AssertCaption("1/b/c");
+
+					using (ops.RenameFolderTemp(p + @"\1\b", "2"))
+					{
+						AssertCaption("1/2/c");
+
+						using (ops.RenameFolderTemp(p + @"\1\2\c", "3"))
+							AssertCaption("1/2/3");
+
+						AssertCaption("1/2/c");
+					}
+
+					AssertCaption("1/b/c");
+				}
+
+				AssertCaption("a/b/c");
+			}
+		}
+
+		public void MoveFolder()
+		{
+			AssertCaption("");
+
+			using (ops.MoveFileTemp(p + @"\" + filename, p + @"\a\b\c"))
+			{
+				AssertCaption("a/b/c");
+
+				using (ops.MoveFolderTemp(p + @"\a\b\c", p))
+				{
+					AssertCaption("c");
+
+					using (ops.MoveFolderTemp(p + @"\c", p + @"\a"))
+					{
+						AssertCaption("a/c");
+
+						using (ops.MoveFolderTemp(p + @"\a\c", p + @"\a\b"))
+							AssertCaption("a/b/c");
+
+						AssertCaption("a/c");
+					}
+
+					AssertCaption("c");
+				}
+
+				AssertCaption("a/b/c");
+			}
+
+			AssertCaption("");
+		}
+
+		public void RenameFile()
+		{
+			AssertCaption("");
+
+			var to = "2" + filename;
+
+			using (ops.RenameFileTemp(p + @"\" + filename, to))
+				AssertCaption(project, "", to);
+
+			AssertCaption("");
+		}
+
+		public void MoveFile()
+		{
+			AssertCaption("");
+
+			using (ops.MoveFileTemp(p + @"\" + filename, p + @"\a"))
+			{
+				AssertCaption("a");
+
+				using (ops.MoveFileTemp(p + @"\a\f.cpp", p + @"\a\b"))
+				{
+					AssertCaption("a/b");
+
+					using (ops.MoveFileTemp(p + @"\a\b\f.cpp", p + @"\a\b\c"))
+					{
+						AssertCaption("a/b/c");
+
+						using (ops.MoveFileTemp(p + @"\a\b\c\f.cpp", p + @"\d\e\f"))
+						{
+							AssertCaption("d/e/f");
+						}
+
+						AssertCaption("a/b/c");
+					}
+
+					AssertCaption("a/b");
+				}
+
+				AssertCaption("a");
+			}
+
+			AssertCaption("");
+		}
+
+		public void OpenFile(Project p)
+		{
+			file.Close();
+
+			file = ops.OpenFile(p, filename);
+			AssertCaption("");
+		}
+
+		public void ToggleExtension()
+		{
+			AssertCaption("");
+
+			using (ops.SetOptionTemp("Enabled", false))
+				Assert.AreEqual(filename, file.Caption);
+
+			AssertCaption("");
+		}
+
+		private void AssertCaption(string s)
+		{
+			AssertCaption(project, s, filename);
+		}
+
+		private void AssertCaption(
+			string project, string folders, string filename)
+		{
+			string expected = project + ":" + folders + ":" + filename;
+			Assert.AreEqual(expected, file.Caption);
+		}
+	}
 }
